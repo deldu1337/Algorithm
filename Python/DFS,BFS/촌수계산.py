f9b396b346fef948af16a0dbee063507769ed2ac
JSH <deldu1337@naver.com>
2023-02-16 15:49:26 +0900

    Python

--- Python/DFS,BFS/촌수계산.py
+++ Python/DFS,BFS/촌수계산.py
@@ -1,35 +1,22 @@
 from collections import deque
 
-n = int(input())
-x, y = map(int, input().split())
-m = int(input())
-arr = [[] * 1 for _ in range(n+1)]
-for i in range(m):
-    a, b = map(int, input().split())
-    arr[a].append(b)
-    arr[b].append(a)
-
-visited = [False] * (n+1)
-def bfs(arr, visited, x, y):
-    visited[y] = True
+def bfs(node):
     queue = deque()
-    cnt = 0
-    queue.append((y))
+    queue.append(node)
     while queue:
-        a = queue.popleft()
-        if a == x:
-            print(cnt)
-            return
-        for v in arr[a]:
-            if not visited[v]:
-                visited[v] = True
-                queue.append((v))
-        cnt += 1
-    print(-1)
-
-if x > y:
-    bfs(arr, visited, x, y)
-else:
-    bfs(arr, visited, y, x)
-
-    
+        node = queue.popleft()
+        for n in graph[node]:
+            if check[n] == 0:
+                check[n] = check[node]+1
+                queue.append(n)
+            
+n = int(input())
+graph = [[] for _ in range(n+1)]
+s, e = map(int, input().split())
+for _ in range(int(input())):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+check = [0]*(n+1)
+bfs(s)
+print(check[e] if check[e] > 0 else -1)
