388a5358601116527cf3c162b2c41807f70a60a2
JSH <deldu1337@naver.com>
2023-02-23 18:10:10 +0900

    Python

---
+++ Python/정렬/일곱 난쟁이.py
@@ -0,0 +1,26 @@
+from sys import stdin
+input = stdin.readline
+arr = []
+for i in range(9):
+    arr.append(int(input()))
+a=-1
+b=-1
+for i in range(9):
+    for j in range(9):
+        if i != j:
+            if (sum(arr) - 100) == (arr[i] + arr[j]):
+                a = i
+                b = j
+                break
+    if a > -1 and b > -1:
+        break
+print(a,b)
+answer = []
+for i in range(9):
+    if i == a or i == b:
+        continue
+    else:
+        answer.append(arr[i])
+answer.sort()
+for i in range(7):
+    print(answer[i])
