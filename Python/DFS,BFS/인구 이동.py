aa3ff4819b14c585174f65511b50497d58dabe8d
JSH <deldu1337@naver.com>
2023-02-25 16:12:35 +0900

    Python

--- Python/DFS,BFS/인구 이동.py
+++ Python/DFS,BFS/인구 이동.py
@@ -1,76 +1,64 @@
-import math
 from collections import deque
-n,L,R = map(int, input().split())
-arr = []
-for i in range(n):
-    arr.append(list(map(int, input().split())))
-visited = [[0] * n for _ in range(n)]
 
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
+n, l, r = map(int, input().split(' '))
+graph = [list(map(int, input().split(' '))) for _ in range(n)]
 
-def bfs(a,b):
+d_x = [-1, 0, 1, 0]
+d_y = [0, 1, 0, -1]
+
+is_move = False
+
+
+def bfs(c_x, c_y, visited, grpah):
+    global is_move
+    people = graph[c_x][c_y]
+    count = 1
     queue = deque()
-    queue.append((a,b))
-    visited[a][b] = 1
-    result = arr[a][b]
-    cnt = 1
+    queue.append((c_x, c_y))
+    visited[c_x][c_y] = True
+    temp = list()
+    temp.append((c_x, c_y))
 
     while queue:
-        x,y = queue.popleft()
+        pop_x, pop_y = queue.popleft()
 
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            n_x = pop_x + d_x[i]
+            n_y = pop_y + d_y[i]
 
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+            if n_x < 0 or n_x >= n or n_y < 0 or n_y >= n:
                 continue
-            if visited[nx][ny] == 0 and abs(arr[nx][ny] - arr[x][y]) >= L and abs(arr[nx][ny] - arr[x][y]) <= R:
-                result += arr[nx][ny]
-                cnt += 1
-                visited[nx][ny] = 1
-                queue.append((nx,ny))
 
-    result = result//cnt
+            if visited[n_x][n_y]:
+                continue
+
+            if l <= abs(grpah[pop_x][pop_y] - grpah[n_x][n_y]) <= r:
+                visited[n_x][n_y] = True
+                queue.append((n_x, n_y))
+                people += graph[n_x][n_y]
+                count += 1
+                temp.append((n_x, n_y))
+
+    move_people = people // count
+
+    if count > 1:
+        is_move = True
+        for x, y in temp:
+            graph[x][y] = move_people
+
+answer = 0
+
+while True:
+    is_move = False
+    visited = [[False] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
-            if visited[i][j] == 1:
-                arr[i][j] = result
+            if not visited[i][j]:
+                bfs(i, j, visited, graph)
 
-    for i in range(n):
-        print(arr[i])
-
-    print()
-
-count = 0
-i = 0
-j = 0
-while True:
-    while True:
-        for k in range(4):
-            nx = i + dx[k]
-            ny = j + dy[k]
-
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-            if abs(arr[nx][ny] - arr[i][j]) >= L and abs(arr[nx][ny] - arr[i][j]) <= R:
-                bfs(i,j)
-                i = -1
-                j = -1
-                count += 1
-                visited = [[0] * n for _ in range(n)]
-                break
-        if j == -1:
-            j = 0
-            break
-        else:
-            j += 1
-            break
-    if i-1 >= n and j >= n:
+    if is_move:
+        answer += 1
+    else:
         break
-    elif i == -1:
-        i = 0
-        continue
-    i += 1
 
-print(count)
+print(answer)
