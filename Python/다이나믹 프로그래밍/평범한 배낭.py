43af0b539f471b13df1df429eece455f49f4d346
JSH <deldu1337@naver.com>
2023-03-01 17:54:58 +0900

    Python

--- Python/다이나믹 프로그래밍/평범한 배낭.py
+++ Python/다이나믹 프로그래밍/평범한 배낭.py
@@ -1,3 +1,12 @@
+#냅색 알고리즘
+#① 현재 배낭의 허용 무게보다 넣을 물건의 무게가 더 크다면 넣지 않는다.
+#② 그렇지 않다면, 다음 중 더 나은 가치를 선택하여 넣는다
+#    2-1) 현재 넣을 물건의 무게만큼 배낭에서 뺀다. 그리고 현재 물건을 넣는다.
+#    2-2) 현재 물건을 넣지않고 이전 배낭 그대로 가지고 간다.
+
+#① j < weight : d[i][j] = d[i-1][j]
+#② d[i][j] = max( d[i-1][ j-weight ]+value ), d[i-1][j] )
+
 from sys import stdin
 input = stdin.readline
 N, K = map(int, input().split())
