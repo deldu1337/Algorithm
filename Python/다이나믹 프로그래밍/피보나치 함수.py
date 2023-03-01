8ad7cd7281e38909f3f4edc2c0b6f00d437caacd
JSH <deldu1337@naver.com>
2023-03-01 11:41:34 +0900

    Python

---
+++ Python/다이나믹 프로그래밍/피보나치 함수.py
@@ -0,0 +1,17 @@
+from sys import stdin
+input = stdin.readline
+d = [[0,0] for _ in range(41)]
+d[0][0] = 1
+d[1][1] = 1
+
+for i in range(2,41):
+    d[i][0] += d[i-2][0]
+    d[i][0] += d[i-2][1]
+    d[i][1] += d[i-1][0]
+    d[i][1] += d[i-1][1]
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    print(d[n][0], d[n][1])
+    
