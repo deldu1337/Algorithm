b2d8f6cae7937c6c5519355c88caa57afa20ad00
JSH <deldu1337@naver.com>
2023-03-01 16:26:35 +0900

    Python

---
+++ Python/다이나믹 프로그래밍/정수 삼각형.py
@@ -0,0 +1,17 @@
+from sys import stdin
+input = stdin.readline
+n = int(input())
+d = []
+for i in range(n):
+    d.append(list(map(int, input().split())))
+
+for i in range(1,n):
+    for j in range(0,i+1):
+        if j==0:
+            d[i][j] += d[i-1][0]
+        elif j==i:
+            d[i][j] += d[i-1][j-1]
+        else:
+            d[i][j] += max(d[i-1][j-1],d[i-1][j])
+
+print(max(d[n-1]))
